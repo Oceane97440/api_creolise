02c6fa3d829162689010c2ebbedaf0275a8656c5
Oceane97440 <sautronoceane98@gmail.com>
2022-07-27 10:05:27 +0400

    test upload icon

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
+server.use(server.static(__dirname + '/public'));
+
 server.set("port", process.env.PORT || 3000);
 
 server.listen(server.get("port"), () => {
