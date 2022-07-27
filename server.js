025b92a0e04ab457e2cb5bed9daaf3cdc0e88980
Oceane97440 <sautronoceane98@gmail.com>
2022-07-27 10:07:57 +0400

    test modif

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
-server.use(server.static(__dirname + '/public'));
 
 server.set("port", process.env.PORT || 3000);
 
